84405271a7f428aae728d4de86a81219c0f62542
emilianospinoso <emilianospinoso@gmail.com>
2023-11-27 12:17:21 -0300

    resolution of invalid parentheses

---
+++ src/a01InvalidParenthesis/Main.java
@@ -0,0 +1,52 @@
+package a01InvalidParenthesis;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+      HAVE TO USE STACK
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Hash table that takes care of the mappings.
+        System.out.println(isValid("()[[]"));
+    }
+
+    public static boolean isValid(String s) {
+        HashMap<Character, Character> mappings = new HashMap<Character, Character>();
+        mappings.put(')', '(');
+        mappings.put('}', '{');
+        mappings.put(']', '[');
+
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
+    }
+
+}
