13e64fc1aa415ac695013a7fa0ea222eb5732b8b
emilianospinoso <emilianospinoso@gmail.com>
2023-12-05 18:02:36 -0300

    refactor

--- src/a02SlidingWindowAVG/SolutionWindow.java
+++ src/a02SlidingWindowAVG/SolutionWindow.java
@@ -4,8 +4,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class SolutionWindow {
-   static Queue<Integer> q1 = new LinkedList<>();
-    static int window =3;
+    static Queue<Integer> q1 = new LinkedList<>();
+    static int window = 3;
 
     public static void main(String[] args) {
         SolutionWindow solution = new SolutionWindow(); // Crear una instancia de la clase Solution
@@ -25,15 +25,15 @@ public class SolutionWindow {
     // Push element x into stack and return sum of the last[WINDOW] items of the queue
     public static int push(int x) {
 
-        int sum=0;
+        int sum = 0;
         q1.add(x);
         while (q1.size() > window) {
             q1.remove();
         }
 
-        for (int num:q1) {
-            sum+=num;
-            sum=sum/window;
+        for (int num : q1) {
+            sum += num;
+            sum = sum / window;
 
         }
         System.out.println(sum);
