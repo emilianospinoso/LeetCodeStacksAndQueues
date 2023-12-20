d4639405ef66f1bebc4733172d5af04c5f80dc1e
emilianospinoso <emilianospinoso@gmail.com>
2023-12-20 07:19:17 -0300

    MAX FTP in 2 minutes

---
+++ src/a10LongestMaxFtp/LongMaxFtpSolution.java
@@ -0,0 +1,10 @@
+package a10LongestMaxFtp;
+
+public class LongMaxFtpSolution {
+    /*
+    i want to in what period i could mantain the max ftp
+     */
+    public static void main(String[] args) {
+   // tengo que crear un ejercicio de sliding window para esto..no se me ocurre ahora.
+    }
+}
---
+++ src/a10LongestSubstrWithoutRepteatingChars/SolutionSlidingWindow.java
@@ -0,0 +1,17 @@
+package a10LongestSubstrWithoutRepteatingChars;
+
+public class SolutionSlidingWindow {
+    /*
+    Given a string s, find the length of the longest substring without repeating characters.
+        Example 1:  Input: s = "abcabcbb"    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+
+        return 0;
+    }
+}
